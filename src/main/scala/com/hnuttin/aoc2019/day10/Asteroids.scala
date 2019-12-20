8fabed8acf710cdffb9baa8a43d7043286330d5d
Hans Nuttin <hans.nuttin@aca-it.be>
2019-12-20 01:32:38 +0100

    day10 (part1 wip)

--- src/main/scala/com/hnuttin/aoc2019/day10/Asteroids.scala
+++ src/main/scala/com/hnuttin/aoc2019/day10/Asteroids.scala
@@ -3,39 +3,74 @@ package com.hnuttin.aoc2019.day10
 class Asteroid(val x: Int, val y: Int) {
 	def detectOtherAsteroids(asteroids: Asteroids, width: Int, height: Int): Int = {
 		val visibleMap = Array.fill(height)(Array.fill(width)(-1))
-		List.range(0, width)
-				.flatMap(x => List.range(0, height).map(y => Tuple2(x, y)))
+		visibleMap(y)(x) = 0
+		List.range(0, height)
+				.flatMap(y => List.range(0, width).map(x => Tuple2(x, y)))
+				.filter(coordinate => !(coordinate._1 == x && coordinate._2 == y))
 				.foreach(coordinate => mark(visibleMap, asteroids, coordinate))
 		visibleMap.flatten.count(v => v == 1)
 	}
 
-	private def mark(visibleMap: Array[Array[Int]], asteroids: Asteroids, coordinate: Tuple2[Int, Int]): Unit = {
+	private def mark(visibleMap: Array[Array[Int]], asteroids: Asteroids, coordinate: (Int, Int)): Unit = {
 		if (asteroids.existsAt(coordinate._1, coordinate._2)) {
-			if (visibleMap(coordinate._1)(coordinate._2) == -1) {
-				visibleMap(coordinate._1)(coordinate._2) = 1
+			if (visibleMap(coordinate._2)(coordinate._1) == -1) {
+				visibleMap(coordinate._2)(coordinate._1) = 1
 			}
-			// mark asteroids out of los as invisible
+			if (coordinate._1 == x) {
+				markInvisibleAboveOrBelow(visibleMap, coordinate)
+			} else if (coordinate._2 == y) {
+				markInvisibleLeftOrRight(visibleMap, coordinate)
+			} else {
+				val rawTravel = Tuple2(coordinate._1 - x, coordinate._2 - y)
+				val travel = calculateMinimalDiagonalTravel(rawTravel)
+				markInvisibleDiagonal(travel, Tuple2(coordinate._1 + travel._1, coordinate._2 + travel._2), visibleMap);
+			}
 		}
 	}
 
-	override def equals(other: Any): Boolean = other match {
-		case that: Asteroid =>
-			(that canEqual this) &&
-					x == that.x &&
-					y == that.y
-		case _ => false
+	@scala.annotation.tailrec
+	private def calculateMinimalDiagonalTravel(travel: (Int, Int)): (Int, Int) = {
+		if (travel._1 % 2 == 0 && travel._2 % 2 == 0) {
+			calculateMinimalDiagonalTravel(Tuple2(travel._1 / 2, travel._2 / 2))
+		} else {
+			travel
+		}
 	}
 
-	def canEqual(other: Any): Boolean = other.isInstanceOf[Asteroid]
-
-	override def hashCode(): Int = {
-		val state = Seq(x, y)
-		state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+	private def markInvisibleAboveOrBelow(visibleMap: Array[Array[Int]], coordinate: (Int, Int)): Unit = {
+		if (coordinate._2 > y) {
+			List.range(coordinate._2 + 1, visibleMap.length).foreach(y => visibleMap(y)(x) = 0)
+		} else {
+			List.range(0, coordinate._2).foreach(y => visibleMap(y)(x) = 0)
+		}
 	}
+
+	private def markInvisibleLeftOrRight(visibleMap: Array[Array[Int]], coordinate: (Int, Int)): Unit = {
+		if (coordinate._1 > x) {
+			List.range(coordinate._1 + 1, visibleMap.head.length).foreach(x => visibleMap(y)(x) = 0)
+		} else {
+			List.range(0, coordinate._1).foreach(x => visibleMap(y)(x) = 0)
+		}
+	}
+
+	@scala.annotation.tailrec
+	private def markInvisibleDiagonal(travel: (Int, Int), coordinate: (Int, Int), visibleMap: Array[Array[Int]]): Unit = {
+		if (coordinate._1 >= 0 && coordinate._1 < visibleMap.head.length && coordinate._2 >= 0 && coordinate._2 < visibleMap.length) {
+			visibleMap(coordinate._2)(coordinate._1) = 0
+			markInvisibleDiagonal(travel, Tuple2(coordinate._1 + travel._1, coordinate._2 + travel._2), visibleMap)
+		}
+	}
+
 }
 
 class Asteroids(val asteroids: List[Asteroid]) {
 	def existsAt(x: Int, y: Int): Boolean = asteroids.exists(a => a.x == x && a.y == y)
+
+	def searchMonitoringAsteroid(width: Int, height: Int): (Asteroid, Int) = {
+		asteroids
+				.map(a => Tuple2(a, a.detectOtherAsteroids(this, width, height)))
+				.maxBy(tuple => tuple._2)
+	}
 }
 
 object Asteroids {
--- src/test/scala/com/hnuttin/aoc2019/day10/AsteroidsTest.scala
+++ src/test/scala/com/hnuttin/aoc2019/day10/AsteroidsTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class AsteroidsTest extends FunSuite {
 
-	test("parse") {
+	test("parse example1") {
 		val asteroids = Asteroids.parse(".#..#\n.....\n#####\n....#\n...##")
 		assert(asteroids.asteroids.length == 10)
 		assert(asteroids.existsAt(1, 0))
@@ -17,6 +17,60 @@ class AsteroidsTest extends FunSuite {
 		assert(asteroids.existsAt(4, 3))
 		assert(asteroids.existsAt(3, 4))
 		assert(asteroids.existsAt(4, 4))
+	}
+
+	test("detectOtherAsteroids example1") {
+		val asteroids = Asteroids.parse(".#..#\n.....\n#####\n....#\n...##")
+		assert(asteroids.asteroids.head.detectOtherAsteroids(asteroids, 5, 5) == 7)
+		assert(asteroids.asteroids(1).detectOtherAsteroids(asteroids, 5, 5) == 7)
+		assert(asteroids.asteroids(2).detectOtherAsteroids(asteroids, 5, 5) == 6)
+		assert(asteroids.asteroids(3).detectOtherAsteroids(asteroids, 5, 5) == 7)
+		assert(asteroids.asteroids(4).detectOtherAsteroids(asteroids, 5, 5) == 7)
+		assert(asteroids.asteroids(5).detectOtherAsteroids(asteroids, 5, 5) == 7)
+		assert(asteroids.asteroids(6).detectOtherAsteroids(asteroids, 5, 5) == 5)
+		assert(asteroids.asteroids(7).detectOtherAsteroids(asteroids, 5, 5) == 7)
+		assert(asteroids.asteroids(8).detectOtherAsteroids(asteroids, 5, 5) == 8)
+		assert(asteroids.asteroids(9).detectOtherAsteroids(asteroids, 5, 5) == 7)
+	}
+
+	test("searchMonitoringAsteroid example1") {
+		val moitoring = Asteroids.parse(".#..#\n.....\n#####\n....#\n...##")
+				.searchMonitoringAsteroid(5, 5)
+		assert(moitoring._1.x == 3)
+		assert(moitoring._1.y == 4)
+		assert(moitoring._2 == 8)
+	}
+
+	test("searchMonitoringAsteroid example2") {
+		val moitoring = Asteroids.parse("......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####")
+				.searchMonitoringAsteroid(10, 10)
+		assert(moitoring._1.x == 5)
+		assert(moitoring._1.y == 8)
+		assert(moitoring._2 == 33)
+	}
+
+	test("searchMonitoringAsteroid example3") {
+		val moitoring = Asteroids.parse("#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.")
+				.searchMonitoringAsteroid(10, 10)
+		assert(moitoring._1.x == 1)
+		assert(moitoring._1.y == 2)
+		assert(moitoring._2 == 35)
+	}
+
+	test("searchMonitoringAsteroid example4") {
+		val moitoring = Asteroids.parse(".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..")
+				.searchMonitoringAsteroid(10, 10)
+		assert(moitoring._1.x == 6)
+		assert(moitoring._1.y == 3)
+		assert(moitoring._2 == 41)
+	}
+
+	test("searchMonitoringAsteroid example5") {
+		val moitoring = Asteroids.parse(".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##")
+				.searchMonitoringAsteroid(20, 20)
+		assert(moitoring._1.x == 11)
+		assert(moitoring._1.y == 13)
+		assert(moitoring._2 == 210)
 	}
 
 }
